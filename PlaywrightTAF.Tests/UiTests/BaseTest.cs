8be13172fe184f1d67779eb12c9177f7b3b12b88
Sahak Hovhannisyan <sahak_hovhannisyan@epam.com>
2026-07-09 20:52:24 +0400

    Add Playwright NUnit test project

---
+++ PlaywrightTAF.Tests/UiTests/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using PlaywrightTAF.Core.Configuration;
+
+namespace PlaywrightTAF.Tests.UiTests;
+
+public abstract class BaseTest : PageTest
+{
+    [SetUp]
+    public async Task SetUpAsync()
+    {
+        await Page.GotoAsync(ConfigurationReader.Current.BaseUrl);
+    }
+
+    [TearDown]
+    public async Task TearDownAsync()
+    {
+        if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            await Page.ScreenshotAsync(new()
+            {
+                Path = $"TestResults/screenshots/{TestContext.CurrentContext.Test.Name}.png",
+                FullPage = true
+            });
+        }
+    }
+}
---
+++ PlaywrightTAF.Tests/UiTests/MainPageTests.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PlaywrightTAF.Core.Configuration;
+using PlaywrightTAF.UI.Pages;
+
+namespace PlaywrightTAF.Tests.UiTests;
+
+public sealed class MainPageTests : BaseTest
+{
+    [Test]
+    public async Task MainPageShouldOpen()
+    {
+        var mainPage = new MainPage(Page);
+
+        bool isLoaded = await mainPage.IsLoadedAsync();
+        string bodyText = await mainPage.GetBodyTextAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(mainPage.CurrentUrl,
+                Does.StartWith(ConfigurationReader.Current.BaseUrl));
+
+            Assert.That(isLoaded, Is.True);
+
+            Assert.That(bodyText, Is.Not.Empty);
+        });
+    }
+}
