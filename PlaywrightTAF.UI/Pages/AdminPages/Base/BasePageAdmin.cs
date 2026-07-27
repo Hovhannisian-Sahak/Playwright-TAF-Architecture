370d940409d31f37b8ed89d4414db8a0d2850509
Sahak Hovhannisyan <sahak_hovhannisyan@epam.com>
2026-07-27 14:51:19 +0400

    create base page for admin and CorporateBrandingPage

--- PlaywrightTAF.UI/Pages/AdminPages/Base/BasePageAdmin.cs
+++ PlaywrightTAF.UI/Pages/AdminPages/Base/BasePageAdmin.cs
@@ -1,6 +1,35 @@
-﻿namespace PlaywrightTAF.UI.Pages.AdminPages.Base;
+﻿using Microsoft.Playwright;
+using PlaywrightTAF.Core.Configuration;
 
-public class BasePageAdmin
+namespace PlaywrightTAF.UI.Pages.AdminPages.Base;
+
+public class BasePageAdmin : BasePage
 {
+    private const string AdminUsersPath = "/web/index.php/admin/viewSystemUsers";
+    private const string CorporateBrandingPath = "/web/index.php/admin/addTheme";
+
+    private ILocator AdminMenuLink => Page.GetByRole(AriaRole.Link, new() { Name = "Admin" });
+    private ILocator CorporateBrandingMenuLink => Page.GetByRole(AriaRole.Link, new() { Name = "Corporate Branding" });
+    protected override string PageUrl => new Uri(new Uri(ConfigurationReader.Current.BaseUrl), AdminUsersPath).ToString();
+    public override async Task<bool> IsLoadedAsync()
+    {
+        return await Page.Locator("text=System Users").IsVisibleAsync();
+    }
+    public BasePageAdmin(IPage page) : base(page)
+    {
+    }
     
+    public async Task OpenAdminPageAsync()
+    {
+        await AdminMenuLink.ClickAsync();
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        await Page.WaitForURLAsync($"**{AdminUsersPath}");
+    }
+
+    public async Task ClickToOpenCorporateBrandingAsync()
+    {
+        await CorporateBrandingMenuLink.ClickAsync();
+        await Page.WaitForURLAsync($"**{CorporateBrandingPath}");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    }
 }
--- PlaywrightTAF.UI/Pages/AdminPages/CorporateBrandingPage.cs
+++ PlaywrightTAF.UI/Pages/AdminPages/CorporateBrandingPage.cs
@@ -1,6 +1,46 @@
-﻿namespace PlaywrightTAF.UI.Pages.AdminPages;
+﻿using Microsoft.Playwright;
+using PlaywrightTAF.Core.Configuration;
+using PlaywrightTAF.UI.Pages.AdminPages.Base;
 
-public class CorporateBrandingPage
+namespace PlaywrightTAF.UI.Pages.AdminPages;
+
+public class AdminCorporateBrandingPage : BasePageAdmin
 {
-    
+    private const string CorporateBrandingPath = "/web/index.php/admin/addTheme";
+
+    ILocator CorporateBrandingHeader => Page.Locator("text=Corporate Branding");
+
+    private ILocator ColorPickerButton => Page.Locator(".orangehrm-color-input-wrapper")
+        .Filter(new() { HasText = "Secondary Font Color" })
+        .Locator(".oxd-color-input-preview");
+    private ILocator ColorPicker => Page.Locator(".oxd-color-picker");
+    private ILocator ColorPickerInput => Page.Locator(".oxd-color-picker")
+        .Locator(".oxd-input");
+
+    private ILocator PublishButton => Page.GetByRole(AriaRole.Button, new() { Name = "Publish" });
+    protected override string PageUrl => new Uri(new Uri(ConfigurationReader.Current.BaseUrl), CorporateBrandingPath).ToString();
+    public override async Task<bool> IsLoadedAsync()
+    {
+        return await CorporateBrandingHeader.IsVisibleAsync();
+    }
+    public AdminCorporateBrandingPage(IPage page) : base(page)
+    {
+    }
+    public async Task ChooseColorAsync()
+    {
+        // click button to choose color
+        await ColorPickerButton.ClickAsync();
+        // waiting color picker to be visible
+        await ColorPicker.WaitForAsync(new()
+        {
+            State = WaitForSelectorState.Visible
+        });
+        // fill color picker
+        await ColorPickerInput.FillAsync("#ff0000");
+    }
+
+    public async Task ClickPublishAsync()
+    {
+        await PublishButton.ClickAsync();
+    }
 }
